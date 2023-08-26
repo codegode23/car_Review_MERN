d4f8a7e0aa77522689d18cbc79715810ee22338f
codegode23 <jasperakabua020@gmail.com>
2023-08-26 04:34:05 +0000

    .env file added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Car Review App With MERN
+
+### server.js is the entry file for the backend app, where the express app is going to be registered 
+
+### npm init -y - create a package.json file in the backend folder. This file keeps track of dependencies and register our own scripts.
+
+### npm install express- install express
+
+### npm install -g nodemon - This installs the nodemon package globally
+
+## Nodemon -
+ nodemon is a tool that helps develop Node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+[Read more here](https://www.npmjs.com/package/nodemon)
+
+## .env file will store all environment variables
+
+## npm install dotenv - installs the dotenv package. this package loads environment variables feom a .env file into the process .env object, available to us globally in a node js environment
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
+//require dotenv package
+require('dotenv').config()
 
+//Require the express package
+const express = require('express');
+
+// const mongoose = require('mongoose');
+
+//start the express app
 const app = express();
 
-app.use(express.json());
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+});
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+//reacting to a request(Routes)
+//get request. '/' specifies the root of the domain 4000. 
+// req - request object, which has info about the request
+// res - response object, used to send a response back to the browser or client
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome To Car Reviews'}) //sends back a json string to the client
+})
+
+
+//Listen for request, and display message if successful
+app.listen(process.env.PORT, () => {
+    console.log('listening on port 4000')
 })
