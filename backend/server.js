beabece16987aaab733afcf3f21d651e2f90ca2f
codegode23 <jasperakabua020@gmail.com>
2023-08-29 21:42:22 +0000

    Server.js updated, app connected to database, Notes file added

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ require('dotenv').config()
 //Require the express package
 const express = require('express');
 
+//require the mongoose package. this will help connect to the database
+
 //require reviews routes
-const reviewRoutes = require('./routes/reviews')
+const reviewRoutes = require('./routes/reviews');
+const { default: mongoose } = require('mongoose');
 
 // const mongoose = require('mongoose');
 
@@ -21,8 +24,21 @@ app.use((req, res, next) => {
 //request /api/reviews to be able to access review routes
 app.use('/api/reviews',reviewRoutes)
 
+//connect to database
+mongoose.connect(process.env.MONGO_URI)
 
-//Listen for request, and display message if successful
+//fire a function when the database is connected to the app
+.then(() => {
+
+//Listen for request, and display message if database connection successful
 app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000')
+    console.log('Connected to the database')
+
+})
+
+})
+
+//catch errors when database connection is not successful
+.catch((error) => {
+    console.log(error)
 })
